a145f25ac666447cf117f2a06419c3c24658537b
Dima Kornieiev <108156304+kornieiev@users.noreply.github.com>
2024-04-24 13:34:31 +0200

    reduce

--- js/8-methods_massive.js
+++ js/8-methods_massive.js
@@ -95,3 +95,23 @@ console.log(`${label}: ${color}`);
 
 // Метод findIndex()
 //
+
+//
+// Метод reduce()
+
+const tweets = [
+  { id: "000", likes: 5, tags: ["js", "nodejs"] },
+  { id: "001", likes: 2, tags: ["html", "css"] },
+  { id: "002", likes: 17, tags: ["html", "js", "nodejs"] },
+  { id: "003", likes: 8, tags: ["css", "react"] },
+  { id: "004", likes: 0, tags: ["js", "nodejs", "react"] }
+];
+
+let sum = tweets.reduce((total, like) => {
+  return (total += like.likes);
+}, 0);
+
+console.log("sum", sum);
+
+// Метод reduce()
+//
