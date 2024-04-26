488a5a5ad97671ae702a610efe0d5ad863f3f6e4
Dima Kornieiev <108156304+kornieiev@users.noreply.github.com>
2024-04-26 10:56:37 +0200

    proto

---
+++ js/10-prototype_class.js
@@ -0,0 +1,61 @@
+// приклад процедурного коду, в якому є змінні і функція для підрахунку результату.
+const baseSalary = 30000;
+const overtime = 10;
+const rate = 20;
+
+const getWage = (baseSalary, overtime, rate) => {
+  return baseSalary + overtime * rate;
+};
+
+// console.log(getWage(baseSalary, overtime, rate));
+
+//
+
+// Використовуємо ООП, зібравши дані в об'єкт employee.
+const employee = {
+  baseSalary: 30000,
+  overtime: 10,
+  rate: 20,
+  getWage() {
+    return this.baseSalary + this.overtime * this.rate;
+  }
+};
+
+// console.log(employee.getWage());
+
+//
+
+// Метод Object.create(obj)
+const animal = {
+  legs: 4
+};
+const dog = Object.create(animal);
+dog.name = "Манго";
+
+// console.log(dog); // { name: 'Манго', __proto__: animal }
+// console.log(animal.isPrototypeOf(dog)); // true
+
+//
+// Репета
+//
+
+//
+//
+
+const objC = {
+  z: 5
+};
+
+const objB = Object.create(objC);
+
+objB.y = 2;
+
+console.log(objC);
+// console.log(objC.hasOwnProperty("z"));
+
+console.log(objB);
+console.log(objB.y);
+console.log(objB.z);
+
+objC.z = 100;
+console.log(objB.z);
