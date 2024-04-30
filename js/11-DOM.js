11681bc28d270733967724c0705c9fcdfdfb0170
Dima Kornieiev <108156304+kornieiev@users.noreply.github.com>
2024-04-30 10:37:44 +0200

    add DOM examples

---
+++ js/11-DOM.js
@@ -0,0 +1,51 @@
+//
+// Навігація по DOM
+
+// console.log("document:", document);
+
+// const body = document.body;
+// console.log("body", body);
+
+// const list = document.querySelector(".list");
+// console.log("list", list);
+
+// const firstListItem = list.firstElementChild;
+// console.log("firstListItem", firstListItem);
+
+// const lastListItem = list.lastElementChild;
+// console.log("lastListItem", lastListItem);
+
+// const listItems = list.children;
+// console.log("listItems", listItems);
+
+// Навігація по DOM
+//
+
+// const listWithId = document.querySelector("#menu");
+// listWithId.style.textTransform = "uppercase";
+// listWithId.style.fontSize = "24px";
+// listWithId.style.listStyle = "none";
+// console.log("listWithId", listWithId);
+
+// const listWithClass = document.querySelector(".menu");
+// console.log("listWithClass", listWithClass);
+
+// const menuItemsByTagName = document.querySelectorAll("li");
+// console.log("menuItemsByTagName", menuItemsByTagName);
+
+// const menuItemsByClass = document.querySelectorAll(".menu-item");
+// console.log("menuItemsByClass", menuItemsByClass);
+
+// const firstMenuItem = document.querySelector(".menu-item");
+// firstMenuItem.style.color = "tomato";
+// console.log("firstMenuItem", firstMenuItem);
+
+//
+//
+//
+//
+//
+//
+
+const link = document.querySelector(".link");
+console.log(link.classList);
