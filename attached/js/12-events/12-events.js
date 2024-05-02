6ac171b326024d74b79a21b50265612f0977e59f
Dima Kornieiev <108156304+kornieiev@users.noreply.github.com>
2024-05-02 11:11:57 +0200

    Подія submit

---
+++ attached/js/12-events/12-events.js
@@ -0,0 +1,80 @@
+//
+// Метод addEventListener()
+const singleBtn = document.querySelector("#single");
+
+const handleClick = () => {
+  console.log("click event listener callback");
+};
+
+singleBtn.addEventListener("click", handleClick);
+
+// ===============================================
+
+const multiBtn = document.querySelector("#multiple");
+
+const firstCallback = () => {
+  console.log("first Callback");
+};
+
+const secondCallback = () => {
+  console.log("second Callback");
+};
+
+const thirdCallback = () => {
+  console.log("third Callback");
+};
+
+multiBtn.addEventListener("click", firstCallback);
+multiBtn.addEventListener("click", secondCallback);
+multiBtn.addEventListener("click", thirdCallback);
+
+// Метод addEventListener()
+//
+
+////
+
+//
+// Метод removeEventListener()
+
+const addListener = document.querySelector(".js-add");
+const removeListener = document.querySelector(".js-remove");
+const targetListener = document.querySelector(".target-btn");
+
+const handleClick2 = () => {
+  console.log("listener works");
+};
+
+addListener.addEventListener("click", () => {
+  console.log("listener added on 'Click me' button");
+  targetListener.addEventListener("click", handleClick2);
+});
+
+removeListener.addEventListener("click", () => {
+  console.log("listener removed from 'Click me' button");
+  targetListener.removeEventListener("click", handleClick2);
+});
+
+// Метод removeEventListener()
+//
+
+////
+
+//
+// Параметр event
+
+const eventBtn = document.querySelector(".event-btn");
+
+eventBtn.addEventListener("click", event => {
+  console.log(event);
+  console.log(event.type);
+  console.log(event.target);
+  // console.log("event.currentTarget", event.currentTarget);
+});
+
+// Параметр event
+//
+
+////
+
+//
+// Подія submit
