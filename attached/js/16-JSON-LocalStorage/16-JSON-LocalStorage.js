29bcab778d8d3455152b9cb4d10574f4a3c012c0
Dima Kornieiev <108156304+kornieiev@users.noreply.github.com>
2024-05-09 23:00:39 +0200

    LS in form

--- attached/js/16-JSON-LocalStorage/16-JSON-LocalStorage.js
+++ attached/js/16-JSON-LocalStorage/16-JSON-LocalStorage.js
@@ -31,3 +31,83 @@
 
 ///
 ///
+
+//
+// JSON
+
+// Object => JSON
+const obj = {
+  name: "Lola",
+  age: 22,
+  student: true,
+  skills: [{ english: "b2" }, { react: true }]
+};
+
+const lola = JSON.stringify(obj);
+// console.log("lola", lola);
+
+//
+
+// JSON => Object
+const lolaJs = JSON.parse(lola);
+// console.log("lolaJs", lolaJs);
+
+// JSON
+//
+
+////
+////
+
+//
+// LocalStorage
+// https://youtu.be/w8vKiU0Hhts?t=327
+
+// запись в LS
+localStorage.setItem("lola-data", lola);
+
+// console.log("localStorage", localStorage.getItem);
+// console.log("localStorage", localStorage.length);
+
+// чтение из LS
+const jsonFromLS = localStorage.getItem("lola-data");
+// console.log("jsonFromLS", jsonFromLS);
+
+const parsedDataFromLS = JSON.parse(jsonFromLS);
+// console.log("parsedDataFromLS:", parsedDataFromLS);
+
+// LocalStorage
+//
+
+////
+////
+
+//
+// Форма отзывов
+// https://youtu.be/w8vKiU0Hhts?t=920
+
+const refs = {
+  form: document.querySelector(".js-form"),
+  textarea: document.querySelector(".js-form textarea")
+};
+
+refs.form.addEventListener("submit", onSubmitClick);
+refs.textarea.addEventListener("input", onTextareaInput);
+
+const textFromLS = localStorage.getItem("textarea");
+refs.textarea.value = textFromLS;
+
+function onTextareaInput(evt) {
+  const text = evt.target.value;
+
+  localStorage.setItem("textarea", text);
+}
+
+function onSubmitClick(evt) {
+  evt.preventDefault();
+
+  const name = evt.target.elements.name.value;
+  const message = evt.target.elements.textarea.value;
+
+  refs.form.reset();
+  localStorage.removeItem("textarea");
+}
