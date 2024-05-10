06c01678738531d44e2382b32b805a325cadb054
Dima Kornieiev <108156304+kornieiev@users.noreply.github.com>
2024-05-10 15:16:03 +0200

    Пример - Оповещение

--- attached/js/17-Timers_Date/17-Timers_Date.js
+++ attached/js/17-Timers_Date/17-Timers_Date.js
@@ -1,25 +1,37 @@
 //
 // setInterval - clearInterval
 
-const loger = (text, intervalTime) => {
-  console.log(`${text}! intervalTima=${intervalTime} - ${Date.now()}`);
-};
-const intervalTime = 3000;
+// const loger = (text, intervalTime) => {
+//   console.log(`${text}! intervalTima=${intervalTime} - ${Date.now()}`);
+// };
+// const intervalTime = 3000;
 
-setInterval(loger, 2000, "hello", intervalTime);
+// setInterval(loger, 2000, "hello", intervalTime);
 
-const startInterval = setInterval(loger, 2000, "start", intervalTime);
+// const startInterval = setInterval(loger, 2000, "start", intervalTime);
 
 //
 
-const stopInteval = Math.random() > 0.3;
-console.log("stopInteval", stopInteval);
-if (stopInteval) {
-  clearInterval(startInterval);
-}
+// const stopInteval = Math.random() > 0.3;
+// console.log("stopInteval", stopInteval);
+// if (stopInteval) {
+//   clearInterval(startInterval);
+// }
 
 // setInterval - clearInterval
 //
 
 ///
 ///
+
+//
+// Пример - Оповещение
+// https://youtu.be/x-Hm2lZcOsM?t=3007
+
+const refs = {
+  notification: document.querySelector("js-alert")
+};
+
+refs.notification.addEventListener("click", onNotificationClick);
+
+function onNotificationClick(evt) {}
