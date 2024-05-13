eb90e95d8afa4493d643f7c7e154da06b36eac15
Dima Kornieiev <108156304+kornieiev@users.noreply.github.com>
2024-05-13 22:35:33 +0200

    Mumbling

--- attached/js/codewars.js
+++ attached/js/codewars.js
@@ -110,7 +110,7 @@ function spinWords(string) {
     newStr.push(word);
   });
 
-  console.log("newStr", newStr.join(" "));
+  // console.log("newStr", newStr.join(" "));
   return newStr.join(" ");
 }
 
@@ -125,3 +125,27 @@ spinWords("Seriously this is the last one"); // "ylsuoireS this is the last one"
 // =================================================================
 
 // Who likes it?
+
+function likes(names) {
+  if (names.length < 1) {
+    return "no one likes this";
+  } else if (names.length === 1) {
+    return `${names[0]} likes this`;
+  } else if (names.length === 2) {
+    return `${names[0]} and ${names[1]} like this`;
+  } else if (names.length === 3) {
+    return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+  } else {
+    return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+  }
+}
+
+// console.log(likes([])); // 'no one likes this';
+// console.log(likes(["Peter"])); // 'Peter likes this';
+// console.log(likes(["Jacob", "Alex"])); // 'Jacob and Alex like this';
+// console.log(likes(["Max", "John", "Mark"])); // 'Max, John and Mark like this';
+// console.log(likes(["Alex", "Jacob", "Mark", "Max"])); // 'Alex, Jacob and 2 others like this';
+
+// =================================================================
+
+// Mumbling
