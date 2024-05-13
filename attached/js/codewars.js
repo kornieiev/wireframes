0c1f59142e79d3aa98882247e211a81960b5ec81
Dima Kornieiev <108156304+kornieiev@users.noreply.github.com>
2024-05-13 19:23:10 +0200

    Who likes it?

--- attached/js/codewars.js
+++ attached/js/codewars.js
@@ -121,3 +121,7 @@ spinWords("This is another test"); // "This is rehtona test"
 spinWords("You are almost to the last test"); // "You are tsomla to the last test"
 spinWords("Just kidding there is still one more"); // "Just gniddik ereht is llits one more"
 spinWords("Seriously this is the last one"); // "ylsuoireS this is the last one"
+
+// =================================================================
+
+// Who likes it?
