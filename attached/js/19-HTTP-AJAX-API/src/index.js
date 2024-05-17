ae7a06642f825cd359b1f8709ce7986546056fdc
Dima Kornieiev <108156304+kornieiev@users.noreply.github.com>
2024-05-17 16:18:17 +0200

    fetch

--- attached/js/19-HTTP-AJAX-API/src/index.js
+++ attached/js/19-HTTP-AJAX-API/src/index.js
@@ -72,21 +72,3 @@ fetch("https://jsonplaceholder.typicode.com/users")
 ////
 
 //
-
-let globalVariable; // undefined
-
-// Initializing data fetching
-fetch("https://jsonplaceholder.typicode.com/users")
-  .then(response => response.json())
-  .then(users => {
-    console.log("users inside then callback: ", users);
-
-    // Writing the result to a global variable
-    globalVariable = users;
-
-    // Everything is ok here, the data is in the variable
-    console.log("globalVariable inside fetch callback: ", globalVariable);
-  });
-
-// No async data here
-console.log("globalVariable outside fetch: ", globalVariable); // undefined
